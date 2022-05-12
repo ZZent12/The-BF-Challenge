448b535e77cdfa505b551bd995291e9564925a65
ZZent12 <92940670+ZZent12@users.noreply.github.com>
2022-05-11 23:10:13 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,35 +1,31 @@
 from flask import Flask, render_template, redirect, url_for, request
 
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return redirect(url_for("backstory"))
 
-def Main():
-    app = Flask(__name__)
+@app.route("/backstory")
+def backstory():
+    return render_template("backstory.html", nav_back="/", nav_next="/principles")
 
-    @app.route("/")
-    def index():
-        return redirect(url_for("backstory"))
-    
-    @app.route("/backstory")
-    def backstory():
-        return render_template("backstory.html", nav_back="/", nav_next="/principles")
-    
-    @app.route("/compVinter")
-    def compVinter():
-        return render_template("compVinter.html", back_to="/backstory")
-    
-    @app.route("/principles")
-    def principles():
-        return render_template("principles.html", nav_back="/backstory", nav_next="defining")
+@app.route("/compVinter")
+def compVinter():
+    return render_template("compVinter.html", back_to="/backstory")
 
-    @app.route("/loops")
-    def loops():
-        return render_template("loops.html", back_to="/principles")
-    
-    @app.route("/defining")
-    def defining():
-        return render_template("defining.html", nav_back="/principles", nav_next="/")
+@app.route("/principles")
+def principles():
+    return render_template("principles.html", nav_back="/backstory", nav_next="defining")
+@app.route("/loops")
+def loops():
+    return render_template("loops.html", back_to="/principles")
 
-    app.run(debug=True)
+@app.route("/defining")
+def defining():
+    return render_template("defining.html", nav_back="/principles", nav_next="/")
+
+    
 
 
 if __name__ == "__main__":
-    Main()
+    app.run(debug=True)
